7a53cc7acd02ecb41df40e80832908333eed5120
SauravAutobit <saurav.autobit@gmail.com>
2025-09-05 14:49:07 +0530

    Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,33 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <>
-      <Header />
-      <Sidebar
-        isSidebarOpen={isSidebarOpen}
-        //   toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
-        setIsSidebarOpen={setIsSidebarOpen}
-      />
-    </>
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div className="main-content">
+        <Sidebar
+          isSidebarOpen={isSidebarOpen}
+          //   toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <div
+          className="page-content"
+          style={{
+            marginLeft: isSidebarOpen ? "0px" : "236px",
+            transition: "margin-left 0.3s",
+          }}
+        >
+          <Outlet />
+        </div>
+      </div>
+    </div>
   );
 }
 
